bb808da1c560ac9c24229dfa8d40afc027d5e360
paul <paulfernandezchomik@gmail.com>
2023-05-13 16:13:11 -0300

    fix NaN error

--- actions.js
+++ actions.js
@@ -1,3 +1,6 @@
+//variable global del texto en pantalla
+const a1=document.getElementById('fname');
+
 
 function reset()
 {
@@ -6,120 +9,11 @@ function reset()
     a1.value='';
 }
 
-
-function n_one()
+function update_text(n)
 {
 	const a1=document.getElementById('fname');
-	a1.value=a1.value+'1';
+	a1.value=a1.value+n;
 }
-
-function n_dos()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'2';
-}
-
-function n_tres()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'3';
-}
-
-function n_for()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'4';
-}
-
-function n_five()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'5';
-}
-
-function n_six()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'6';
-}
-
-function n_7()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'7';
-}
-
-function n_8()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'8';
-}
-
-function n_9()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'9';
-}
-
-function n_0()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'0';
-}
-
-function n_pi()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'π';
-}
-
-
-function n_point()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'·';
-}
-
-function n_por()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'×';
-}
-
-function n_menos()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'-';
-}
-
-function n_mas()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'+';
-}
-
-function n_divide()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'÷';
-}
-
-function n_parentesis_der()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+'(';
-}
-
-function n_parentesis_izq()
-{
-	const a1=document.getElementById('fname');
-	a1.value=a1.value+')';
-}
-
-/*function separar(texto)
-{
-	let texto1=
-}*/
 
 function igual()
 {
@@ -127,6 +21,14 @@ function igual()
 	let    a=a1.value;
 	a=a.replace('×', '*');
 	a=a.replace('÷', '/');
+	a=a.replace('π', '3.14159');
 	a=eval(a);
 	a1.value=+a;
+}
+
+function igualENTER(event)
+{
+	if (event.keyCode === 13) {	
+		igual()
+	}
 }
---
+++ prueabas.js
@@ -0,0 +1,10 @@
+//pruebas de valiables globales
+
+var n1=2;
+var n2=3;
+
+function suma()
+{
+	suma=n1+n2
+	console.log(suma);
+}
