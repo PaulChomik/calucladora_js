a767c52049d0d488b1599e4b6341707337768e5a
PaulChomik <75439631+PaulChomik@users.noreply.github.com>
2024-10-18 16:23:37 -0300

    Create actions_cientific.js

---
+++ actions_cientific.js
@@ -0,0 +1,46 @@
+function n_one()
+{
+	const a1=document.getElementById('fname');
+	a1.value=a1.value+'1';
+}
+function n_dos()
+{
+	const a1=document.getElementById('fname');
+	a1.value=a1.value+'2';
+}
+function n_tres()
+{
+	const a1=document.getElementById('fname');
+	a1.value=a1.value+'3';
+}
+function n_for()
+{
+	const a1=document.getElementById('fname');
+	a1.value=a1.value+'4';
+}
+function n_five()
+{
+	const a1=document.getElementById('fname');
+	a1.value=a1.value+'5';
+}
+function n_six()
+{
+	const a1=document.getElementById('fname');
+	a1.value=a1.value+'6';
+}
+function n_7()
+{
+	const a1=document.getElementById('fname');
+	a1.value=a1.value+'7';
+}
+function n_8()
+{
+	const a1=document.getElementById('fname');
+	a1.value=a1.value+'8';
+}
+function n_9()
+{
+	const a1=document.getElementById('fname');
+	a1.value=a1.value+'9';
+}
+function n_0()
