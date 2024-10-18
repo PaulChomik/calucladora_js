64fa1f268facae2b4880efa81a3631605daa37de
PaulChomik <75439631+PaulChomik@users.noreply.github.com>
2024-10-18 16:24:37 -0300

    Update actions_cientific.js

--- actions_cientific.js
+++ actions_cientific.js
@@ -1,3 +1,37 @@
+//variable global del texto en pantalla
+const a1=document.getElementById('fname');
+
+
+function reset()
+{
+	//const doc=getDocument('index.html')
+    const a1=document.getElementById('fname');
+    a1.value='';
+}
+
+function update_text(n)
+{
+	const a1=document.getElementById('fname');
+	a1.value=a1.value+n;
+}
+
+function igual()
+{
+	const a1=document.getElementById('fname');
+	let    a=a1.value;
+	a=a.replace('×', '*');
+	a=a.replace('÷', '/');
+	a=a.replace('π', '3.14159');
+	a=eval(a);
+	a1.value=+a;
+}
+
+function igualENTER(event)
+{
+	if (event.keyCode === 13) {	
+		igual()
+	}
+}
 function n_one()
 {
 	const a1=document.getElementById('fname');
